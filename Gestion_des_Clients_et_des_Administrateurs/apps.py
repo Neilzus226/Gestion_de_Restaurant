6aed90abde526d86140ddfc561f53ab75cb962ec
Neilzus226 <kelebonajuliens@gmail.com>
2025-12-16 21:57:52 +0100

    Ajout du module Gestion des Clients et des Administateurs

---
+++ Gestion_des_Clients_et_des_Administrateurs/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GestionDesClientsEtDesAdministrateursConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Gestion_des_Clients_et_des_Administrateurs'
