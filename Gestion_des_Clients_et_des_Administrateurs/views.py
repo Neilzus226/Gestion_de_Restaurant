2b30cf0d7114a801f4982502982c4b52a81359be
DABIRE Christian <cdabire268@gmail.com>
2025-12-27 17:43:21 +0000

    Vue et Templates Html

--- Gestion_des_Clients_et_des_Administrateurs/views.py
+++ Gestion_des_Clients_et_des_Administrateurs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Profil
+def liste_utilisateurs(request): 
+    utilisateurs = Profil.objects.all()
+    return
+render(request, 'gestion/liste_utilisateurs.html',{'utilisateur': utilisateurs})
