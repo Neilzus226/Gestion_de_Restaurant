84644e586e33ede1604e5348950b918bd1e3132a
DABIRE Christian <cdabire268@gmail.com>
2025-12-27 15:59:33 +0000

    finalisation du modèle de gestion d'utilisateur

--- Gestion_des_Clients_et_des_Administrateurs/models.py
+++ Gestion_des_Clients_et_des_Administrateurs/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.db import models 
 
 # Create your models here.
+django.contrib.auth.models 
+import User 
+
+class Profil(models.Model):
+#on lit ce profil à un compte utilisateur Django
+    user= models.OneTOneField(User, on_delete=models.CASCADE)
+    #Définition des deux rôles
+    ROLE_CHOICES = [('ADMIN','Administrateur'),('CLIENT','Client'),]
+    role = models.CharField(max_length=10,choices=ROLE_CHOICES, default='CLIENT')
+    telephone = models.CharField(max_length=20,blank=True)
+    def __str__(self):
+        return f"{self.user.username}({self.role})"    
+
