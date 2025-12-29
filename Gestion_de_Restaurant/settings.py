d4c4fc632fd2700d658eea276870dabd7b3dcffc
DABIRE Christian <cdabire268@gmail.com>
2025-12-29 11:58:31 +0000

    base de donnée

--- Gestion_de_Restaurant/settings.py
+++ Gestion_de_Restaurant/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Gestion_des_Clients_et_des_Administrateurs',
 ]
 
 MIDDLEWARE = [
