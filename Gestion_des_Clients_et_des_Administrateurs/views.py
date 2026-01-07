10ed2f7b194a888b42ed81e88550c64e84e96b22
DABIRE Christian <cdabire268@gmail.com>
2026-01-07 13:36:17 +0000

    modification

--- Gestion_des_Clients_et_des_Administrateurs/views.py
+++ Gestion_des_Clients_et_des_Administrateurs/views.py
@@ -2,6 +2,35 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Profil
+
+#code Python de "Ajouter un utilisateur"
 def liste_utilisateurs(request): 
     utilisateurs = Profil.objects.all()
     return render(request, 'gestion/liste_utilisateurs.html',{'utilisateurs': utilisateurs})
+from django.contrib.auth.models import User
+from .models import Profil
+def ajouter_utilisateur(request):
+    if request.method == 'POSt':
+        user = User.objects.create_user(
+username=request.POST['username'],
+password=request.POST['password'])
+        Profil.objects.create(user=user,
+role=request.POST['role'],
+telephone=request.POST['telephone'])
+        return redirect('liste_utilisateurs')
+    return render(request,'gestion/ajouter_utilisateur.html')  
+
+#code Python de "Modifier un utilisateur"  
+def modifier_utilisateur(request,id):
+    profil = Profil.objects.get(id=id)
+    if request.method == 'POST':
+        profil.role = request.POST['role']
+        profil.telephon = request.POST['telephone']
+        profil.save()
+        return redirect('liste_utilisateurs')
+        return render(request,'gestion/modifier_utilisateur.html',{'profil':profil})
+    
+#code python de "supprimer utilisateur"
+def supprimer_utilisateur(request,id):
+    profil = Profil.objects.get(id=id)
+    return redirect('liste_utilisateur')    
