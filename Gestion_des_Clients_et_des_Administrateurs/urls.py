758f36dac6fb038641b7071c97edc1493693528b
DABIRE Christian <cdabire268@gmail.com>
2026-01-07 10:51:27 +0000

    modification

--- Gestion_des_Clients_et_des_Administrateurs/urls.py
+++ Gestion_des_Clients_et_des_Administrateurs/urls.py
@@ -3,4 +3,7 @@ from . import views
 urlpatterns = [
     #lecture de l'adresse/liste/de la vue
     path('liste/', views.liste_utilisateurs, name='liste_utilisateurs'),
+    path('utilisateur/AJOUTER/',views.AJOUTER_utilisateur, name='utilisateur_add'),
+    path('utilisateur/MODIFIER/<int:id>/', views.MODIFIER_utilisateur, name='utilisateur_edit'),
+    path('utilisateur/SUPPRIMER/<int:id>/', views.SUPPRIMER_utilisateur, name='utilisateur_delete'),
 ]
